e448531fe56c8364ec81d79bb06c4301279429b7
Sergei Sumarokov <7047457+kompotkot@users.noreply.github.com>
2023-01-07 16:14:15 +0300

    Changes for conda env support at windows

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON=C:\Users\<your_username>\anaconda3\envs\automatic\python.exe
 set GIT=
-set VENV_DIR=
+set VENV_DIR=C:\Users\<your_username>\anaconda3\envs\automatic
 set COMMANDLINE_ARGS=
 
 call webui.bat
